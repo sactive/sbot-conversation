38f8433ea1e1c38c8b3448ca885113d0afc2b44a
shipengqi <104604657@qq.com>
2018-07-03 22:09:48 +0800

    ut

--- test/dialog.test.js
+++ test/dialog.test.js
@@ -3,9 +3,14 @@ const {expect} = require('chai');
 const conversationManager = require('..');
 const Helper = require('hubot-test-helper');
 const robot = new Robot('hubot/src/adapters', 'shell');
+const helper = new Helper('./mock/scripts.js');
 
 let MANAGER_USER = null;
 let MANAGER_ROOM = null;
+let room = null;
+let switchBoard = null;
+let userName = 'pooky';
+let receiverUserId= null;
 describe('Hubot conversation manager tests', function () {
   before(function () {
     
--- test/mock/scripts.js
+++ test/mock/scripts.js
@@ -1,3 +1,4 @@
+const {DYNAMIC_SCHEMA_MOCK, JSON_SCHEMA_MOCK} = require('./schemas');
 module.exports = function (robot) {
-  
+  robot.respond('')
 };
